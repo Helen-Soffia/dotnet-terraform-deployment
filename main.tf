e8a2f44b4c1b783a674418571816bb64668b72d4
Soffia <helensoffiajp@gmail.com>
2024-10-11 12:16:24 +0530

    clean up

--- main.tf
+++ main.tf
@@ -1,26 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your AWS region
+  region = "us-east-1"  # Specify the AWS region
 }
 
-variable "app_s3_bucket" {}
-variable "app_s3_key" {}
-
-resource "aws_s3_bucket_object" "app_version" {
-  bucket = var.app_s3_bucket
-  key    = var.app_s3_key
-  source = "dotnet-app.zip"
-}
-
-# # Create an S3 bucket for storing the application bundle
-# resource "aws_s3_bucket" "app_bucket" {
-#   bucket = "my-dotnet-app-bucket"  # Change this to a unique bucket name
-# }
-
-# # Upload the zipped application to S3 using the new resource
+# # Hardcode the existing S3 bucket and object
 # resource "aws_s3_object" "app_version" {
-#   bucket = aws_s3_bucket.app_bucket.bucket
-#   key    = "dotnet-app.zip"  # The name of the file in the S3 bucket
-#   source = "dotnet-app.zip"  # Source is the zipped file created above
+#   bucket = "elasticbeanstalk-us-east-1-825765409649"  # Hardcoded S3 bucket name
+#   key    = "EvalWebApp/AWSDeploymentArchive_EvalWebApp_v20241002070149.zip"  # Hardcoded file path in S3 bucket
+#   source = "EvalWebApp.zip"  # Local path to the file you are uploading
 # }
 
 # Create an Elastic Beanstalk Application
@@ -33,8 +19,8 @@ resource "aws_elastic_beanstalk_application" "dotnet_app" {
 resource "aws_elastic_beanstalk_application_version" "app_version" {
   name        = "v1-${timestamp()}"  # Use a unique version name
   application = aws_elastic_beanstalk_application.dotnet_app.name
-  bucket      = aws_s3_bucket.app_bucket.bucket
-  key         = aws_s3_object.app_version.key
+  bucket      = "dotnet-sample-github"  # Use the existing S3 bucket
+  key         = "sample-eval.zip"  # Key of the existing S3 object
 }
 
 resource "aws_elastic_beanstalk_environment" "dotnet_environment" {
